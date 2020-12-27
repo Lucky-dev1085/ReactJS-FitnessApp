76de33c055853b9c86ffba2259edabc7a5e8c919
Lucky-dev1085 <guerrero.alvaro1085@gmail.com>
2020-12-27 13:08:50 -0500

    update new app

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.css'
-import App from './components/App'
-
-const container = document.getElementById('root')
-
-ReactDOM.render(<App />, container)
